35eca0fe256c68cc1e5e8ff9aec6f9ae9daff9c9
Aleksandr Kasatkin <aleksandr_kasatkin@epam.com>
2026-01-21 13:12:34 +0100

    finished

---
+++ starter.py
@@ -0,0 +1,8 @@
+from task.app import main, rag
+
+if __name__ == "__main__":
+    main(rag)
+
+# python starter.py
+# What safety precautions should be taken to avoid exposure to excessive microwave energy?
+# What do you think about the dinosaur era? Why did they die?
